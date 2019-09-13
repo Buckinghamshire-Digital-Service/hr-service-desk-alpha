d328700202758af1058537318f8890dbd37a740e
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2019-09-13 17:03:22 +0100

    Fix: #25 - adding extra role=contentinfo
    https://github.com/Buckinghamshire-Digital-Service/hr-service-desk-alpha/issues/25

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = props => {
         <Heading text='Need to get in touch?' className='h3'/>
       </div>
       <Divider />
-      <nav className='navbar container' role='navigation' aria-label='Footer navigation'>
+      <nav className='navbar container' role='navigation' aria-label='Footer navigation' role='contentinfo'>
         <Navigation items={footer} /> 
       </nav>
     </footer>
