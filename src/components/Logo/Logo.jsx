8c6e989e199725012627258754623404b32209a2
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2019-09-20 09:20:01 +0100

    Removing Link

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Svg from '../Svg/Svg.jsx'
 
 const Logo = props => {
   return (
     <div className='logo'>
-      <Link to='/' className='logo__link' title='Buckinghamshire Council HR Service Desk homepage'>
+      <a href='/' className='logo__link' title='Buckinghamshire Council HR Service Desk homepage'>
         <Svg url={props.url} alt='Buckinghamshire Council'/>
-      </Link>
+      </a>
     </div>
   )
 }
