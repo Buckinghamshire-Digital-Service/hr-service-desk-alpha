8fbab9d8d51e8ddf3b4a8821e688da018be157c8
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2019-09-24 13:29:52 +0100

    Fixing errors

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
+import get from 'lodash/get'
 import Icon from '../components/Icon/Icon.jsx'
 import Layout from '../components/Layout/Layout.jsx'
 import Main from '../components/Main/Main.jsx'
@@ -13,25 +14,27 @@ import Accent from '../components/Accent/Accent.jsx'
 class ErrorPage extends React.PureComponent {
   render() {
 
-  const { pathname } = props.location
-  let path = pathname.split('/').filter(v => v !== '')
-  path.pop()
-  path = path.join('/')
-  const title = props.parent && props.parent.title !== undefined ? props.parent.title : 'Homepage'
+    const site = get(this.props, 'data.site.siteMetadata')
+    console.log(this.props.location)
+  // const { pathname } = props.location
+  // let path = pathname.split('/').filter(v => v !== '')
+  // path.pop()
+  // path = path.join('/')
+  // const title = props.parent && props.parent.title !== undefined ? props.parent.title : 'Homepage'
 
 
     
     return (
-      <Layout location={this.props.location} hasSearch className='full-width' hero={page.hero} ga={site.gaConfig.id} map={null}>
-        <Helmet title={page.metaTitle} description={page.metaDescription}/>
+      <Layout location={this.props.location} hasSearch className='full-width' hero={null} ga={site.gaConfig.id} map={null}>
+        <Helmet title='4040 error page not found' description='404 error page not found'/>
         <Main className='full-width'>
-          {this.props.location && <Breadcrumb location={this.props.location} parent={page.parent} className='container'/>}
+          <Breadcrumb location={this.props.location} parent={{'title': 'Homepage'}} className='container'/>
           <div className='container'>
             <PageTitle text='404 error - this page is missing'/>
             <div className='panel panel--inverted panel--padding-small'>
-              <Accent className='accent--loud accent--shallow'>
+              <Accent className='accent--loud accent--shallow accent--separated'>
                 <p className='lead'>
-                  Sorry, the term "<strong>{this.state.searched}</strong>" returned no results : (
+                  Sorry, we couldn't find the page you were looking for
                 </p>                  
               </Accent>
               <p className='lead'>
@@ -41,7 +44,7 @@ class ErrorPage extends React.PureComponent {
               <ul className='list bullet'>
                 <li className='list__item'>Use a different search term</li>
                 <li className='list__item'>Search for something else</li>
-                <li className='list__item'>Go <Link to='/'>back to your homepage</Link> and try again</li>
+                <li className='list__item'>Go <Link to='/' className='link-text underlined'>back to your homepage</Link> and try again</li>
               </ul>
             </div>
           </div>
@@ -52,3 +55,17 @@ class ErrorPage extends React.PureComponent {
 }
 
 export default ErrorPage
+
+export const errorPageQuery = graphql`
+  query ErrorPage {
+    site {
+      siteMetadata {
+        title
+        basePath
+        gaConfig {
+          id
+        }
+      }
+    }
+  }
+`
