a4579b9c82a710dddf8e886032f71c5ab4918913
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2019-09-25 15:14:38 +0100

    Fix: #65 - download link
    https://github.com/Buckinghamshire-Digital-Service/hr-service-desk-alpha/issues/65

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -5,7 +5,7 @@ import { Event } from '../GoogleAnalytics/GoogleAnalytics'
 const Download = props => {
   return (
     <div className={`panel panel--inverted panel--padding-small`}>
-      <Link to='/downloads' className='lead flush padding--h-flush d-block' onClick={() => Event('Download items link','Click')}>
+      <Link to='/downloads' className='download-link lead flush padding--h-flush d-block' onClick={() => Event('Download items link','Click')}>
         <span>Go to download index</span>
         <span className='download download--small spaced-left'></span>
       </Link>
