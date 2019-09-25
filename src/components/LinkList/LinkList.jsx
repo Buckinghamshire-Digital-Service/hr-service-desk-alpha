ed6865445a363bab272514f0c5459f7efd79d90f
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2019-09-25 11:58:03 +0100

    Fix: #88 - home button changes
    https://github.com/Buckinghamshire-Digital-Service/hr-service-desk-alpha/issues/88

--- src/components/LinkList/LinkList.jsx
+++ src/components/LinkList/LinkList.jsx
@@ -25,9 +25,8 @@ const LinkList = props => {
               </Link>
               {content && <Heading type='p' text={content} />}
               <ButtonGroup className='button-group--fixed'>
-                <Button url={link} className='btn--has-radius btn--primary btn--full-width' aria-hidden  clickHandler={() => ctaClick(v.title)}>
-                  Find out more
-                  <span className='is-sr-only'> about {v.title}</span>
+                <Button url={link} className='btn--has-radius btn--primary btn--full-width' clickHandler={() => ctaClick(v.title)} aria-label={`Find out more about ${v.title}`}>
+                  More on {v.title}
                 </Button>
               </ButtonGroup>
             </div>
