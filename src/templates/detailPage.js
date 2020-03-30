3fb08094062544cd83476987086ec23eb4283619
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2020-03-30 11:56:04 +0100

    Tweak to collapsible listing

--- src/templates/detailPage.js
+++ src/templates/detailPage.js
@@ -23,17 +23,18 @@ class PageTemplate extends React.PureComponent {
     const post = get(this.props, 'data.contentfulPage')
     const parent = post.parentPage ? {'parentPage': true} : null
     let mediaLinks = post.collapsibleLinks ? post.collapsibleLinks
-      .filter(v => v.mediaFile !== undefined)
-      .map(v => v.mediaFile && v.mediaFile.file.url)
+      .filter(v => v['mediaLink'] !== null || v.mediaFile !== undefined)
+      .map(v => v['mediaLink'] || (v.mediaFile && v.mediaFile.file.url))
       .reduce((a,b) => a.concat(b), []): []
-      
+            
       mediaLinks = mediaLinks
         .concat(post.mediaLink || [])
-        .filter(item => mediaLinks.indexOf(item) < 0)
+        // .filter(item => mediaLinks.indexOf(item) < 0)
         .map(v => {
           v['mediaLink'] = v['mediaLink'] !== null ? v['mediaLink'] : (v.mediaFile && v.mediaFile.file) && 'https:' + v.mediaFile.file.url        
           return v
         })
+
 
     return (
       <Layout location={this.props.location} hasSearch className='full-width' hero={post.hero} ga={site.gaConfig.id} map={map} {...parent}>
