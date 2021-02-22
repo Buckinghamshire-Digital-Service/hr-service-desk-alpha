57628244bb0c6e48707a946609046c098cb550d1
Dave Martin <dave.martin@cxpartners.co.uk>
2021-02-22 13:51:08 +0000

    re-adding host variable back in

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ require('dotenv').config({
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: process.env.CONTENTFUL_HOST,
   downloadLocal: true
 }
 
