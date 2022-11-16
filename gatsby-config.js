3bf794799adfd5f60b576760b2430d835768da5c
Andy Brace <git-hub@openbook.uk.com>
2022-11-16 17:23:55 +0000

    adding contentful env in order to test out redirect page changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   // host: process.env.CONTENTFUL_HOST,
+  environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
   downloadLocal: true
 }
 
